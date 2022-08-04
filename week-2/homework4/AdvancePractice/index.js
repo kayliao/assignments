8a27aa589474ffa2f665ff0a72e5c0be1f6513b2
Kay Liaw <111190goodgoodeat@gmail.com>
2022-08-04 11:44:17 +0800

    update advance practice with get method

--- week-2/homework4/AdvancePractice/index.js
+++ week-2/homework4/AdvancePractice/index.js
@@ -8,27 +8,50 @@ app.use(body_parser.urlencoded({ extended: false }));
 
 app.set("view engine", "pug");
 
+/*
+by using get query string in url.
+can only use url query string by get method.
+get method send message by query
+post method send message by body
+*/
+
 app.get("/myName", (request, response) => {
 	const name = request.cookies.username;
-	if (name) {
-		response.render("index", { name });
-	} else {
-		response.redirect("/trackName");
-	}
+	response.render("index", { name });
 });
 
 app.get("/trackName", (request, response) => {
-	const name = request.cookies.username;
-	if (name) {
-		response.redirect("/myName");
-	} else {
-		response.render("index");
-	}
-});
-
-app.post("/trackName", (request, response) => {
-	response.cookie("username", request.body.username);
+	const name = request.query.username;
+	response.cookie("username", name);
 	response.redirect("/myName");
 });
+
+/*
+by using post -> only request.body
+(if it's get method -> sending data in url)
+*/
+
+// app.get("/myName", (request, response) => {
+// 	const name = request.cookies.username;
+// 	if (name) {
+// 		response.render("index", { name });
+// 	} else {
+// 		response.redirect("/trackName");
+// 	}
+// });
+
+// app.get("/trackName", (request, response) => {
+// 	const name = request.cookies.username;
+// 	if (name) {
+// 		response.redirect("/myName");
+// 	} else {
+// 		response.render("index");
+// 	}
+// });
+
+// app.post("/trackName", (request, response) => {
+// 	response.cookie("username", request.body.username);
+// 	response.redirect("/myName");
+// });
 
 app.listen(3000);
