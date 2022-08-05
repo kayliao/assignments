5772e45c902e72ade484dbce86d363dd8395a90a
Kay Liaw <111190goodgoodeat@gmail.com>
2022-08-05 10:20:34 +0800

    adding test practice list todo

---
+++ week-2/homework3/Test/ToDoList/function.js
@@ -0,0 +1,31 @@
+const enterPlace = document.querySelector(".enterPlace");
+const toDo = document.querySelector(".toDo");
+
+enterPlace.addEventListener("keypress", function (event) {
+	// If the user presses the "Enter" key on the keyboard
+	if (event.key === "Enter") {
+		// Cancel the default action, if needed
+		// event.preventDefault();
+		// Trigger the button element with a click
+		// document.getElementById("myBtn").click();
+
+		let value = enterPlace.value;
+
+		let result = `<input type="checkbox" class="newTaskItem" />
+    <label class="strikethrough">${value}</label>`;
+		if (toDo.lastElementChild == null) {
+			toDo.innerHTML = result;
+			enterPlace.value = "";
+		} else {
+			let li = document.createElement("li");
+			// li.className = "item";
+			li.innerHTML = result;
+			insert_after(li, toDo.lastElementChild);
+			enterPlace.value = "";
+		}
+	}
+});
+
+function insert_after(new_node, existing_node) {
+	existing_node.parentNode.insertBefore(new_node, existing_node.nextSibling);
+}
