11919afa248e5df2134ad5ee9780361384139c3f
Kay Liaw <111190goodgoodeat@gmail.com>
2022-08-05 10:20:08 +0800

    fix texting style

--- week-2/homework3/AdvancePractice/advance.js
+++ week-2/homework3/AdvancePractice/advance.js
@@ -3,26 +3,26 @@ let count_page = 1;
 let first_time = true;
 
 function ajax(src) {
-  return fetch(src)
-    .then(checkStatus)
-    .then((response) => response.json())
-    .catch((error) => console.log("There is an error: ", error));
+	return fetch(src)
+		.then(checkStatus)
+		.then((response) => response.json())
+		.catch((error) => console.log("There is an error: ", error));
 }
 
 function checkStatus(response) {
-  if (response.ok) {
-    return Promise.resolve(response);
-  } else {
-    return Promise.reject(new Error(response.statusText));
-  }
+	if (response.ok) {
+		return Promise.resolve(response);
+	} else {
+		return Promise.reject(new Error(response.statusText));
+	}
 }
 
 function render(data) {
-  console.log(data);
-  if (first_time) {
-    print_result.innerHTML = data
-      .map((info) => {
-        let content = `
+	console.log(data);
+	if (first_time) {
+		print_result.innerHTML = data
+			.map((info) => {
+				let content = `
             <li class="item">
             <div class="title">
                 <a href="">${info.name}</a>
@@ -31,19 +31,19 @@ function render(data) {
             <p class="content">${info.description}</p>
             <div class="tag_item">
           `;
-        let alink = "";
-        for (let i = 0; i < info.topics.length; i++) {
-          alink += `<a href="">${info.topics[i]}</a>`;
-        }
+				let alink = "";
+				for (let i = 0; i < info.topics.length; i++) {
+					alink += `<a href="">${info.topics[i]}</a>`;
+				}
 
-        let result = content + alink + `</div></li>`;
-        first_time = false;
-        return result;
-      })
-      .join(" ");
-  } else {
-    for (let a = 0; a < data.length; a++) {
-      let content = `
+				let result = content + alink + `</div></li>`;
+				first_time = false;
+				return result;
+			})
+			.join(" ");
+	} else {
+		for (let a = 0; a < data.length; a++) {
+			let content = `
         <div class="title">
             <a href="">${data[a].name}</a>
             <span>${data[a].visibility}</span>
@@ -51,34 +51,34 @@ function render(data) {
         <p class="content">${data[a].description}</p>
         <div class="tag_item">
       `;
-      let alink = "";
-      for (let i = 0; i < data[a].topics.length; i++) {
-        alink += `<a href="">${data[a].topics[i]}</a>`;
-      }
+			let alink = "";
+			for (let i = 0; i < data[a].topics.length; i++) {
+				alink += `<a href="">${data[a].topics[i]}</a>`;
+			}
 
-      let result = content + alink + `</div>`;
+			let result = content + alink + `</div>`;
 
-      let li = document.createElement("li");
-      li.className = "item";
-      li.innerHTML = result;
-      insert_after(li, print_result.lastElementChild);
-    }
-  }
+			let li = document.createElement("li");
+			li.className = "item";
+			li.innerHTML = result;
+			insert_after(li, print_result.lastElementChild);
+		}
+	}
 
-  console.log(print_result.innerHTML);
+	console.log(print_result.innerHTML);
 }
 
 function insert_after(new_node, existing_node) {
-  existing_node.parentNode.insertBefore(new_node, existing_node.nextSibling);
+	existing_node.parentNode.insertBefore(new_node, existing_node.nextSibling);
 }
 
 ajax("https://api.github.com/orgs/facebook/repos?per_page=5&page=1").then(
-  (data) => render(data)
+	(data) => render(data)
 );
 
 function showMoreContent() {
-  count_page++;
-  ajax(
-    `https://api.github.com/orgs/facebook/repos?per_page=5&page=${count_page}`
-  ).then((data) => render(data));
+	count_page++;
+	ajax(
+		`https://api.github.com/orgs/facebook/repos?per_page=5&page=${count_page}`
+	).then((data) => render(data));
 }
