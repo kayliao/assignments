2866f57c58f0364e9e4d75ff1be7b50dcbc98aed
Kay Liaw <111190goodgoodeat@gmail.com>
2022-08-05 15:25:44 +0800

    fix style by prettier

--- week-2/homework3/RWD page action/action.js
+++ week-2/homework3/RWD page action/action.js
@@ -1,28 +1,28 @@
 const welcomeBlock = document.querySelector(".welcomeBox");
 
 welcomeBlock.addEventListener(
-  "click",
-  () => (welcomeBlock.innerHTML = "<h1>Have a Good Time!</h1>")
+	"click",
+	() => (welcomeBlock.innerHTML = "<h1>Have a Good Time!</h1>")
 );
 
 function showMoreContent() {
-  const hidden = document.querySelector(".hiddenContent");
-  const butt = document.querySelector(".btnaction button");
-  if (hidden.style.display === "block") {
-    hidden.style.display = "none";
-    butt.textContent = "Show More Content";
-  } else {
-    hidden.style.display = "block";
-    butt.textContent = "Hide Content";
-  }
+	const hidden = document.querySelector(".hiddenContent");
+	const butt = document.querySelector(".btnaction button");
+	if (hidden.style.display === "block") {
+		hidden.style.display = "none";
+		butt.textContent = "Show More Content";
+	} else {
+		hidden.style.display = "block";
+		butt.textContent = "Hide Content";
+	}
 }
 
 function showList() {
-  const open = document.querySelector(".menu");
-  open.style.display = "block";
+	const open = document.querySelector(".menu");
+	open.style.display = "block";
 }
 
 function closeMenu() {
-  const close = document.querySelector(".menu");
-  close.style.display = "none";
+	const close = document.querySelector(".menu");
+	close.style.display = "none";
 }
